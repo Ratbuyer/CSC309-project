ac00dae44a7c90cc18da0608e2735d45ac010324
Youngjun Cho <youngjun.cho@mail.utoronto.ca>
2022-12-03 23:55:48 -0500

    Fix CORS error

--- PF/backend/group_8958/settings.py
+++ PF/backend/group_8958/settings.py
@@ -46,11 +46,13 @@ INSTALLED_APPS = [
     "classes",
     "studios",
     "subscription",
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -153,3 +155,6 @@ REST_FRAMEWORK = {
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(days=30),
 }
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True

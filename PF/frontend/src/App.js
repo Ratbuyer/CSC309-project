6a7f909c753ef96a5f17503f2ecddb67035b269b
Caules Ge <caules.ge@mail.utoronto.ca>
2022-12-04 16:00:16 -0500

    update

--- PF/frontend/src/App.js
+++ PF/frontend/src/App.js
@@ -2,11 +2,13 @@
 import './App.css';
 import {BrowserRouter,Routes, Route} from 'react-router-dom';
 import MainPage from './components/mainPage';
+import Studios from './components/Studios';
 import MainNavigation from './components/Navigation/MainNavigation'
 
 function App() {
   let routes = ( <Routes>
         <Route path="/" element={<MainPage />} exact></Route>
+        <Route path="/studios" element={<Studios />}></Route>
       </Routes>)
 
 

4c7bf585b25b9b62efa9d09aa4f675185c7b5b1f
Caules Ge <caules.ge@mail.utoronto.ca>
2022-12-05 01:55:54 -0500

    update

--- PF/frontend/src/components/Studios/StudioDetails.js
+++ PF/frontend/src/components/Studios/StudioDetails.js
@@ -1,22 +1,7 @@
 
 import React, {useState, useEffect} from "react";
 import { useParams } from 'react-router-dom';
-import AppBar from '@mui/material/AppBar';
-import Button from '@mui/material/Button';
-import CameraIcon from '@mui/icons-material/PhotoCamera';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import CssBaseline from '@mui/material/CssBaseline';
-import Grid from '@mui/material/Grid';
-import Stack from '@mui/material/Stack';
-import Box from '@mui/material/Box';
 
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-
-import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 
 
